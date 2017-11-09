2bed6d966fd1c1eaf7626d713a72963a0d8cdf09
David Arthur <mumrah@gmail.com>
2017-11-09 15:53:06 -0500

    Some basic UI working

--- src/FrontPanel.cpp
+++ src/FrontPanel.cpp
@@ -3,61 +3,86 @@
 #define ENCODER_DO_NOT_USE_INTERRUPTS
 #include <Encoder.h>
 #include "ascii.h"
+#include "FrontPanel.h"
 
 LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 Encoder myEnc(A1, A2);
 
-struct UIState {
-  long oldPosition;
-  long last_turn;
-  byte lastButton;
-  bool button_down = false;
-  long double_click_time;
-  bool in_double_click = false;
-}
-
 UIState uiState;
 
-long oldPosition  = -999;
-long last_turn;
-byte lastButton = 1;
-bool button_down = false;
-long double_click_time;
+void renderUI() {
+  if(uiState.ui_mode == UI_MODE_RUN) {
+    char buf[7], out[9];
+    memset(buf, 0, 7);
+    memset(out, 0, 9);
+    ultoa(uiState.display_frequency, buf, DEC);
 
-uint32_t tune_step_hz = 10000;
-bool in_double_click = false;
-bool in_fine_tune = false;
+    strcat(out, "");
+    out[0] = buf[0];
+    strcat(out, ".");
+    strncat(out, &buf[1], 3);
+    strcat(out, ".");
+    strncat(out, &buf[4], 3);
+    lcd.setCursor(0, 0);
+    lcd.print("A ");
+    lcd.print(out);
+    lcd.print("  LSB");
 
-long freq;
-
-void printFreq(long freq) {
-  char buf[7], out[9];
-  memset(buf, 0, 7);
-  memset(out, 0, 9);
-  ultoa(freq, buf, DEC);
-
-  strcat(out, "");
-  out[0] = buf[0];
-  strcat(out, ".");
-  strncat(out, &buf[1], 3);
-  strcat(out, ".");
-  strncat(out, &buf[4], 3);
-  lcd.setCursor(0, 0);
-  lcd.print("A ");
-  lcd.print(out);
-  lcd.print("  LSB");
+    // S-meter
+    lcd.setCursor(0, 1);
+    uint8_t bar;
+    switch(uiState.tune_step_hz) {
+      case 100000:
+        bar = 4;
+        break;
+      case 10000:
+        bar = 5;
+        break;
+      case 1000:
+        bar = 6;
+        break;
+      case 100:
+        bar = 8;
+        break;
+    }
+    for(uint8_t i=0; i<9; i++) {
+      uint8_t glyph = (i / 3) << 1;
+      if(i == bar) {
+        glyph |= 0x01;
+      }
+      lcd.write(glyph);
+      //lcd.print(glyph);
+    }
+  } else if(uiState.ui_mode == UI_MODE_CONFIG_MENU || uiState.ui_mode == UI_MODE_CONFIG_SELECT) {
+    lcd.setCursor(0, 0);
+    switch(uiState.menu_idx) {
+      case 0:
+        lcd.print("VFO: ");
+        lcd.print(uiState.menu_vfo);
+        lcd.print("          ");
+        break;
+      default:
+      lcd.print("Menu ");
+      lcd.print(uiState.menu_idx);
+      lcd.print("        ");
+    }
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+  }
 }
 
 void setupFrontPanel(long initialFrequency) {
-  freq = initialFrequency;
+  uiState.display_frequency = initialFrequency;
   pinMode(A3, INPUT_PULLUP);
   lcd.begin(16, 2);
   lcd.createChar(0, s1);
-  lcd.createChar(1, s4);
-  lcd.createChar(2, s7);
-  lcd.createChar(3, rx);
-  lcd.createChar(4, tx);
-  lcd.createChar(5, over);
+  lcd.createChar(1, s1_bar);
+  lcd.createChar(2, s4);
+  lcd.createChar(3, s4_bar);
+  lcd.createChar(4, s7);
+  lcd.createChar(5, s7_bar);
+  lcd.createChar(6, rx);
+  lcd.createChar(7, tx);
 
   lcd.setCursor(0, 1);
   lcd.write((uint8_t)0);
@@ -65,64 +90,149 @@ void setupFrontPanel(long initialFrequency) {
   lcd.write((uint8_t)0);
   lcd.write((uint8_t)1);
   lcd.write((uint8_t)1);
-  lcd.write((uint8_t)1);
+  lcd.write((uint8_t)6);
   lcd.write((uint8_t)2);
   lcd.write((uint8_t)2);
   lcd.write((uint8_t)2);
-  lcd.write((uint8_t)5);
-  lcd.write((uint8_t)5);
-  lcd.write((uint8_t)5);
-  lcd.write((uint8_t)5);
-  lcd.write((uint8_t)5);
+  lcd.print((char)0xFF);
+  lcd.print((char)0xFF);
+  lcd.print((char)0xFF);
+  lcd.print((char)0xFF);
+  lcd.print((char)0xFF);
   lcd.print(" ");
-  lcd.write((uint8_t)3);
+  lcd.write((uint8_t)6);
 }
 
+// Read from knobs and buttons, determine which UI events have occurred
 void pollFrontPanel() {
-
-  long newPosition = myEnc.read();
-  long posDiff = newPosition - oldPosition;
-  long tune_adjust = tune_step_hz;
-  if(abs(posDiff) > 3) {
-    if(button_down) {
-      tune_adjust = tune_step_hz / 10;
-      in_fine_tune = true;
+  long new_encoder_position = myEnc.read();
+  long encoder_diff = new_encoder_position - uiState.last_encoder_position;
+  if(abs(encoder_diff) > 3) {
+    // gone a full turn
+    if(uiState.is_button_down) {
+      uiState.is_press_and_turn = true;
     }
-    if(posDiff > 0) {
-      freq += tune_adjust;
-    } else {
-      freq -= tune_adjust;
+    uint8_t turns = abs(encoder_diff) / 3;
+    for(uint8_t i=1; i<turns; i++) {
+      onKnobRotate(encoder_diff > 0);
     }
-    printFreq(freq);
-    //setFrequency(VFO_A, freq);
-    last_turn = millis();
-    oldPosition = newPosition;
+    onKnobRotate(encoder_diff > 0);
+    uiState.last_turn_ms = millis();
+    uiState.last_encoder_position = new_encoder_position;
   }
 
-  byte newButton = digitalRead(A3);
-  if(lastButton == 1 && newButton == 0) {
+  byte new_button_state = digitalRead(A3);
+  if(uiState.last_button_state == 1 && new_button_state == 0) {
     Serial.println("onButtonDown");
-    lastButton = newButton;
-    button_down = true;
-    if((millis() - double_click_time) < 500) {
+    uiState.last_button_state = new_button_state;
+    uiState.is_button_down = true;
+    if((millis() - uiState.last_button_down_ms) < 300) {
       Serial.println("onDoubleClick");
-      in_double_click = true;
+      uiState.is_double_click = true;
+      onDoubleClick();
     }
-    double_click_time = millis();
-  } else if(lastButton == 0 && newButton == 1) {
-    if(!in_double_click && !in_fine_tune) {
+    uiState.last_button_down_ms = millis();
+  } else if(uiState.last_button_state == 0 && new_button_state == 1) {
+    if(!uiState.is_double_click && !uiState.is_press_and_turn) {
       Serial.println("onClick");
-      tune_step_hz = tune_step_hz / 10;
-      if(tune_step_hz == 10) {
-        tune_step_hz = 100000;
-      }
+      onClick();
     }
     Serial.println("onButtonUp");
-    lastButton = newButton;
-    button_down = false;
-    in_double_click = false;
-    in_fine_tune = false;
+    uiState.last_button_state = new_button_state;
+    uiState.is_button_down = false;
+    uiState.is_double_click = false;
+    uiState.is_press_and_turn = false;
   } else {
-    lastButton = newButton;
+    uiState.last_button_state = new_button_state;
+  }
+
+  renderUI();
+}
+
+void doTune(bool is_clockwise) {
+  long tune_adjust = uiState.tune_step_hz;
+  if(uiState.is_press_and_turn) {
+    tune_adjust = 1;
+  }
+  if(is_clockwise > 0) {
+    uiState.display_frequency += tune_adjust;
+  } else {
+    uiState.display_frequency -= tune_adjust;
+  }
+}
+
+void onKnobRotate(bool is_clockwise) {
+  switch(uiState.ui_mode) {
+    case UI_MODE_RUN:
+      doTune(is_clockwise);
+      break;
+    case UI_MODE_CONFIG_MENU:
+      if(is_clockwise) {
+        if(uiState.menu_idx == 9) {
+          uiState.menu_idx = 0;
+        } else {
+          uiState.menu_idx += 1;
+        }
+      } else {
+        if(uiState.menu_idx == 0) {
+          uiState.menu_idx = 9;
+        } else {
+          uiState.menu_idx -= 1;
+        }
+      }
+      break;
+    case UI_MODE_CONFIG_SELECT:
+      switch(uiState.menu_idx) {
+        case 0:
+          // VFO
+          if((uiState.menu_vfo == 'A' && is_clockwise) || (uiState.menu_vfo == 'S' && !is_clockwise)) {
+            uiState.menu_vfo = 'B';
+          } else if((uiState.menu_vfo == 'B' && is_clockwise) || (uiState.menu_vfo == 'A' && !is_clockwise)) {
+            uiState.menu_vfo = 'S';
+          } else if((uiState.menu_vfo == 'S' && is_clockwise) || (uiState.menu_vfo == 'B' && !is_clockwise)) {
+            uiState.menu_vfo = 'A';
+          }
+          break;
+      }
+      break;
+  }
+}
+
+void onButtonDown() {
+
+}
+
+void onButtonUp() {
+
+}
+
+void onDoubleClick() {
+  switch(uiState.ui_mode) {
+    case UI_MODE_RUN:
+      uiState.ui_mode = UI_MODE_CONFIG_MENU;
+      break;
+    default:
+      uiState.ui_mode = UI_MODE_RUN;
+      uiState.menu_idx = 0;
+      break;
+  }
+}
+
+void onClick() {
+  switch(uiState.ui_mode) {
+    case UI_MODE_RUN:
+      // Change tuning step
+      uiState.tune_step_hz = uiState.tune_step_hz / 10;
+      if(uiState.tune_step_hz == 10) {
+        uiState.tune_step_hz = 100000;
+      }
+      break;
+    case UI_MODE_CONFIG_MENU:
+      // Select sub-menu
+      uiState.ui_mode = UI_MODE_CONFIG_SELECT;
+      break;
+    case UI_MODE_CONFIG_SELECT:
+      uiState.ui_mode = UI_MODE_CONFIG_MENU;
+      break;
   }
 }
--- src/FrontPanel.h
+++ src/FrontPanel.h
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 
+#define UI_MODE_RUN 0x00
+#define UI_MODE_CONFIG_MENU 0x01
+#define UI_MODE_CONFIG_SELECT 0x02
+
+
 struct UIState {
   long last_encoder_position;
   long last_turn_ms;
@@ -7,11 +12,24 @@ struct UIState {
   byte last_button_state;
 
   bool is_button_down = false;
-  bool in_double_click = false;
+  bool is_double_click = false;
+  bool is_press_and_turn = false;
+
+  byte ui_mode = UI_MODE_RUN;
+  // Run mode state
+  long display_frequency;
+  uint32_t tune_step_hz = 10000;
+
+  // Config mode state
+  uint8_t menu_idx = 0;
+  char menu_vfo = 'A';
 };
 
 void setupFrontPanel(long initialFrequency);
 void pollFrontPanel();
 
-void onClick(UIState uiState);
-void onDoubleClick(UIState uiState);
+void onKnobRotate(bool is_clockwise);
+void onClick();
+void onDoubleClick();
+void onButtonDown();
+void onButtonUp();
